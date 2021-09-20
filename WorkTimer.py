3f9b91589cfa3cd777ed8527a7ec779bb6493b11
haohao1331 <haohao1331@gmail.com>
2021-09-20 13:20:37 -0400

    reformatted to class

--- WorkTimer.py
+++ WorkTimer.py
@@ -2,48 +2,49 @@ from tkinter import *
 import time
 from tkinter import messagebox
 
-root = Tk()
-# root.geometry("500x200")
-root.title("Work Timer")
-title = Label(root, text="Work Timer", padx=100, pady=30).grid(row=0, column=0)
-timeText = StringVar()
-timer = Label(root, textvariable=timeText, padx=100, pady=30).grid(row=1, column=0)
 
-def StopTimer():
-    global running, startTime
-    stopTime = time.time()
-    running = False
-    response = messagebox.askquestion("You Are Done!", "You worked for {}!\nDo you want to resume?".format(timeText.get()))
-    print(response)
-    if response == "yes":
-        running = True
-        startTime = time.time() - stopTime + startTime
-        UpdateTimer()
-    if response == "no":
-        running = True
-        startTime = time.time()
-        UpdateTimer()
+class Timer:
+    def __init__(self):
+        self.running = True
+        self.startTime = time.time()
+        self.root = Tk()
+        # root.geometry("500x200")
+        self.root.title("Work Timer")
+        self.title = Label(self.root, text="Work Timer", padx=100, pady=30).grid(row=0, column=0)
+        self.timeText = StringVar()
+        self.timer = Label(self.root, textvariable=self.timeText, padx=100, pady=30).grid(row=1, column=0)
+        self.stop = Button(self.root, text="I'm Done!", padx=100, pady=10, command=self.StopTimer).grid(row=3)
+        self.UpdateTimer()
+        self.root.mainloop()
+        
+    def StopTimer(self):
+        stopTime = time.time()
+        self.running = False
+        response = messagebox.askquestion("You Are Done!", "You worked for {}!\nDo you want to resume?".format(self.timeText.get()))
+        print(response)
+        if response == "yes":
+            self.running = True
+            self.startTime = time.time() - stopTime + self.startTime
+            self.UpdateTimer()
+        if response == "no":
+            self.running = True
+            self.startTime = time.time()
+            self.UpdateTimer()
+    
+    def UpdateTimer(self):
+        if not self.running: 
+            return
+        hours = FormatedTime(str(int(time.time() - self.startTime) // 3600))
+        minutes = FormatedTime(str((int(time.time() - self.startTime) // 60) % 60))
+        seconds = FormatedTime(str(int(time.time() - self.startTime) % 60))
+        self.timeText.set(hours + ":" + minutes + ":" + seconds)
+        self.root.after(1000, self.UpdateTimer)
 
-stop = Button(root, text="I'm Done!", padx=100, pady=10, command=StopTimer).grid(row=3)
-
-running = True
-def UpdateTimer():
-    if not running: 
-        return
-    hours = FormatedTime(str(int(time.time() - startTime) // 3600))
-    minutes = FormatedTime(str((int(time.time() - startTime) // 60) % 60))
-    seconds = FormatedTime(str(int(time.time() - startTime) % 60))
-    timeText.set(hours + ":" + minutes + ":" + seconds)
-    root.after(1000, UpdateTimer)
 
 def FormatedTime(x):
     if len(x) < 2:
         return "0" + x
     else:
         return x
-
-startTime = time.time()
-
-UpdateTimer()
-
-root.mainloop()
+    
+timer = Timer()
