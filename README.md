69565963d3dbb10598b4185bd18ac6b5063af1ee
haohao1331 <haohao1331@gmail.com>
2021-10-06 01:51:40 -0400

    build for mac, hook up trello API

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # WorkTimer
 Simple program to help you keep track of your work time! 
 Using it by creating keyboard shortcuts on your system to drastically improve your efficiency. 
+
+# Create env.py file
+```
+KEY="your_trello_key_here"
+TOKEN="your_trello_token_here"
+CHECKLIST_ID="checklist_id"
+```
+
+# Install on Mac
+https://py2app.readthedocs.io/en/latest/tutorial.html#create-a-setup-py-file
--- WorkTimer.py
+++ WorkTimer.py
@@ -1,16 +1,30 @@
 from tkinter import *
-import time
 from tkinter import messagebox
+from datetime import datetime
+from env import *
+import time
+import requests
 
+key = KEY
+token = TOKEN
+checklistId = CHECKLIST_ID
+checkListUrl = f'https://api.trello.com/1/checklists/{checklistId}/checkItems'
+
+# print(checkListUrl)
+# print(key, token, checklistId)
+
+timeout = 5
 
 class Timer:
     def __init__(self):
         self.running = True
+        self.lastRecordedDate = datetime.today().strftime('%Y-%m-%d')
         self.startTime = time.time()
         self.root = Tk()
         # root.geometry("500x200")
         self.root.title("Work Timer")
-        self.title = Label(self.root, text="Work Timer", padx=100, pady=30).grid(row=0, column=0)
+        self.title = Entry(self.root)
+        self.title.grid(row=0, column=0)
         self.timeText = StringVar()
         self.timer = Label(self.root, textvariable=self.timeText, padx=100, pady=30).grid(row=1, column=0)
         self.stop = Button(self.root, text="I'm Done!", padx=100, pady=10, command=self.StopTimer).grid(row=3)
@@ -18,19 +32,49 @@ class Timer:
         self.root.mainloop()
         
     def StopTimer(self):
-        stopTime = time.time()
+        if len(self.title.get()) == 0:
+            return
         self.running = False
-        response = messagebox.askquestion("You Are Done!", "You worked for {}!\nDo you want to resume?".format(self.timeText.get()))
-        print(response)
-        if response == "yes":
-            self.running = True
-            self.startTime = time.time() - stopTime + self.startTime
-            self.UpdateTimer()
-        if response == "no":
-            self.running = True
-            self.startTime = time.time()
-            self.UpdateTimer()
-    
+        try:
+            query = {
+                'name': f'{TrelloFormatTime(self.timeText.get())} {self.title.get()}',
+                'pos': 'bottom',
+                'key': key,
+                'token': token
+            }
+            response = requests.request(
+                "POST",
+                checkListUrl,
+                params=query,
+                timeout=timeout
+            )
+            messagebox.showinfo("You Are Done!", f"You worked for {self.timeText.get()}!")
+            print(response.text)
+            
+            newRecordDate = datetime.today().strftime('%Y-%m-%d')
+            if(newRecordDate != self.lastRecordedDate):
+                self.lastRecordedDate = newRecordDate
+                query = {
+                    'name': str(self.lastRecordedDate),
+                    'pos': 'bottom',
+                    'key': key,
+                    'token': token
+                }
+                response = requests.request(
+                    "POST",
+                    checkListUrl,
+                    params=query,
+                    timeout=timeout
+                )
+        except (requests.ConnectionError, requests.Timeout) as exception:
+            messagebox.showinfo("No Connection", f"You worked for {self.timeText.get()}! Note this in your notepad. ")
+        
+        # response = messagebox.askquestion("You Are Done!", "You worked for {}!\nDo you want to resume?".format(self.timeText.get()))
+        self.title.delete(0, 'end')
+        self.running = True
+        self.startTime = time.time()
+        self.UpdateTimer()
+        
     def UpdateTimer(self):
         if not self.running: 
             return
@@ -46,5 +90,14 @@ def FormatedTime(x):
         return "0" + x
     else:
         return x
+    
+def TrelloFormatTime(x : str):
+    (hours, minutes, seconds) = [int(i) for i in x.split(':')]
+    # print(hours, minutes, seconds)
+    minutes += int(seconds >= 30)
+    hours = str(hours + minutes // 60)
+    minutes = str(minutes % 60)
+    return f'{FormatedTime(hours)}:{FormatedTime(minutes)}'
+    
     
 timer = Timer()
