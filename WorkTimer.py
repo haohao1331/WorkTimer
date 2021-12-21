388df8cca5c4648a8facb689306913c3ca95b10a
haohao1331 <haohao1331@gmail.com>
2021-12-21 01:52:09 -0500

    store local working, trello pending test

--- WorkTimer.py
+++ WorkTimer.py
@@ -1,116 +1,246 @@
 from tkinter import *
 from tkinter import messagebox
-from datetime import datetime
+from datetime import datetime, timedelta
 from env import *
+import openpyxl as opx
 import time
 import requests
+import os
+import json
 
-key = KEY
-token = TOKEN
-cardId = CARD_ID
+CARD_URL = f'https://api.trello.com/1/cards/{CARD_ID}/checklists'
 
-cardUrl = f'https://api.trello.com/1/cards/{cardId}/checklists'
+API_TIMEOUT = 5
+SAVE_TO_LOCAL = True
 
-# print(checkListUrl)
-# print(key, token, checklistId)
-
-timeout = 5
+class TimerSettings:
+    def __init__(self, settingsFilePath, 
+                  lastRecordedDatetime = None, 
+                  latestRecordFilePath = '', 
+                  lastRecordWorksheetIndex = 0):
+        self.lastRecordedDatetime = lastRecordedDatetime
+        self.latestRecordFilePath = latestRecordFilePath
+        self.lastRecordWorksheetIndex = lastRecordWorksheetIndex
+        self.settingsFilePath = settingsFilePath
+    
+    def UpdateSettings(self, 
+                       lastRecordedDatetime = None, 
+                       latestRecordFilePath : str = '', 
+                       lastRecordWorksheetIndex : int = -1):
+        lrdt = lastRecordedDatetime if lastRecordedDatetime else self.lastRecordedDatetime
+        settings = {
+            'lastRecordedDatetime': GetStoreDatetimeString(lrdt),
+            'latestRecordFilePath': latestRecordFilePath if latestRecordFilePath != '' else self.latestRecordFilePath,
+            'lastRecordWorksheetIndex' : lastRecordWorksheetIndex if lastRecordWorksheetIndex != -1 else self.lastRecordWorksheetIndex, 
+        }
+        with open(self.settingsFilePath, 'w') as s:
+            json.dump(settings, s, indent=4)
+    
+    def FetchSettingsFromJson(self):
+        with open(self.settingsFilePath) as s:
+            # fetch settings data
+            settings = json.load(s)
+            self.lastRecordedDatetime = RetrieveDatetimeFromString(settings['lastRecordedDatetime'])
+            self.latestRecordFilePath = settings['latestRecordFilePath']
+            self.lastRecordWorksheetIndex = settings['lastRecordWorksheetIndex']
+    
+    def __str__(self):
+        return str(vars(self))
 
 class Timer:
     def __init__(self):
+        #Saves
+        self.dirPath = os.path.expanduser('~/Documents/WorkTimer')
+        self.settingsFilePath = os.path.join(self.dirPath, 'settings.json')
+        self.settings = TimerSettings(self.settingsFilePath)
+        
+        # Timer
         self.running = True
-        self.lastRecordedDate = datetime.today().strftime('%Y-%m-%d')
+        self.lastRecordedDateString = None
         self.startTime = time.time()
+        self.startDatetime = datetime.today()
+        self.endDatetime = None
+        
+        # UI
         self.root = Tk()
-        # root.geometry("500x200")
         self.root.title("Work Timer")
         self.title = Entry(self.root)
         self.title.grid(row=0, column=0)
         self.timeText = StringVar()
         self.timer = Label(self.root, textvariable=self.timeText, padx=100, pady=30).grid(row=1, column=0)
         self.stop = Button(self.root, text="I'm Done!", padx=100, pady=10, command=self.StopTimer).grid(row=3)
+        
+        # launch
+        self.StartUpCheckSaves()
+        print(self.settings)
         self.UpdateTimer()
         self.root.mainloop()
-        
+    
     def StopTimer(self):
         if len(self.title.get()) == 0:
             return
         self.running = False
+        self.endDatetime = datetime.today()
+        
+        if SAVE_TO_LOCAL:
+            self.RecordToLocal()
+        else:
+            self.RecordToTrello()
+        
+        self.title.delete(0, 'end')
+        self.running = True
+        self.startTime = time.time()
+        self.startDatetime = datetime.today()
+        self.UpdateTimer()
+
+# ----------------------- Trello option -------------------------------
+    def GetMostRecentChecklistId(self):
+        query = {
+            'key': KEY,
+            'token': TOKEN
+        }
+        response = requests.get(
+            CARD_URL,
+            params=query
+        )
+        return min(response.json(), key=lambda x: x['pos'])['id']
+    
+    def RecordToTrello(self):
         try:
             checklistId = self.GetMostRecentChecklistId()
             checklistUrl = f'https://api.trello.com/1/checklists/{checklistId}/checkItems'
             query = {
                 'name': f'{TrelloFormatTime(self.timeText.get())} {self.title.get()}',
                 'pos': 'bottom',
-                'key': key,
-                'token': token
+                'key': KEY,
+                'token': TOKEN
             }
             response = requests.request(
                 "POST",
                 checklistUrl,
                 params=query,
-                timeout=timeout
+                timeout=API_TIMEOUT
             )
             messagebox.showinfo("You Are Done!", f"You worked for {self.timeText.get()}!")
             print(response.text)
             
-            newRecordDate = datetime.today().strftime('%Y-%m-%d')
-            if(newRecordDate != self.lastRecordedDate):
-                self.lastRecordedDate = newRecordDate
+            newRecordDateString = datetime.today().strftime('%Y-%m-%d')
+            if(newRecordDateString != self.lastRecordedDateString):
+                self.lastRecordedDateString = newRecordDateString
                 query = {
-                    'name': str(self.lastRecordedDate),
+                    'name': self.lastRecordedDateString,
                     'pos': 'bottom',
-                    'key': key,
-                    'token': token
+                    'key': KEY,
+                    'token': TOKEN
                 }
                 response = requests.request(
                     "POST",
                     checklistUrl,
                     params=query,
-                    timeout=timeout
+                    timeout=API_TIMEOUT
                 )
         except (requests.ConnectionError, requests.Timeout) as exception:
             messagebox.showinfo("No Connection", f"You worked for {self.timeText.get()}! Note this in your notepad. ")
-        
-        # response = messagebox.askquestion("You Are Done!", "You worked for {}!\nDo you want to resume?".format(self.timeText.get()))
-        self.title.delete(0, 'end')
-        self.running = True
-        self.startTime = time.time()
-        self.UpdateTimer()
+    
+# ----------------------- Local option -------------------------------
+    def RecordToLocal(self):
+        # check if need to create anything
+        lrdt = self.settings.lastRecordedDatetime.date()
+        if lrdt:
+            if lrdt < GetClosestMonday():
+                self.CreateNewRecordFile()
+            if lrdt and lrdt < datetime.today().date():
+                self.CreateNewRecordSheet()
+                
+        self.AddRecord()
+        messagebox.showinfo("You Are Done!", f"You worked for {self.timeText.get()}!")
         
     def UpdateTimer(self):
         if not self.running: 
             return
-        hours = FormatedTime(str(int(time.time() - self.startTime) // 3600))
-        minutes = FormatedTime(str((int(time.time() - self.startTime) // 60) % 60))
-        seconds = FormatedTime(str(int(time.time() - self.startTime) % 60))
+        now = time.time()
+        hours = TwoDigitNumber(str(int(now - self.startTime) // 3600))
+        minutes = TwoDigitNumber(str((int(now - self.startTime) // 60) % 60))
+        seconds = TwoDigitNumber(str(int(now - self.startTime) % 60))
         self.timeText.set(hours + ":" + minutes + ":" + seconds)
         self.root.after(1000, self.UpdateTimer)
     
-    def GetMostRecentChecklistId(self):
-        query = {
-            'key': key,
-            'token': token
-        }
-        response = requests.get(
-            cardUrl,
-            params=query
-        )
-        return min(response.json(), key=lambda x: x['pos'])['id']
+    def StartUpCheckSaves(self):
+        if not os.path.isdir(self.dirPath):
+            os.mkdir(self.dirPath)
+        if not os.path.isfile(self.settingsFilePath):
+            self.CreateNewRecordFile()
+            return
+        
+        # if the setting file already exist
+        self.settings.FetchSettingsFromJson()
+        lastMonday = GetClosestMonday()
+        assert lastMonday.weekday() == 0
+        lrd = self.settings.lastRecordedDatetime.date()
+        today = datetime.today().date()
+        if lrd < lastMonday:   # need a new file to record things
+            self.CreateNewRecordFile()
+        else: # lrd >= lastMonday
+            if lrd < today:
+                self.CreateNewRecordSheet()
+            else:
+                # no need to create worksheet or update settings
+                pass
+            
     
+    def CreateNewRecordFile(self, date = None):
+        date = date if date else datetime.today()
+        todayString = date.strftime('%Y-%m-%d')
+        recordFileName = f"{todayString}_Work_Record.xlsx"
+        recordFilePath = os.path.join(self.dirPath, recordFileName)
+        # https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
+        newWorksheetName = f"{date.strftime('%m-%d %A')}"   # 06-09 Friday
+        
+        newRecordFile = opx.Workbook()
+        worksheet = newRecordFile.active
+        worksheet.title = newWorksheetName
+        CreateHeader(worksheet)
+        newRecordFile.save(filename=recordFilePath)
+        
+        self.settings.UpdateSettings(latestRecordFilePath=recordFilePath, 
+                                lastRecordWorksheetIndex=0,
+                                lastRecordedDatetime=datetime.today())
+        
+    def CreateNewRecordSheet(self, date = None):
+        date = date if date else datetime.today()
+        newWorksheetName = f"{date.strftime('%m-%d %A')}"   # 06-09 Friday
+        record = opx.load_workbook(self.settings.latestRecordFilePath)
+        newSheet = record.create_sheet(newWorksheetName)
+        CreateHeader(newSheet)
+        record.save(filename=self.settings.latestRecordFilePath)
+        
+        self.settings.UpdateSettings(lastRecordWorksheetIndex=(len(record.sheetnames)-1))
+        
+    def AddRecord(self):
+        recordFile = opx.load_workbook(self.settings.latestRecordFilePath)
+        recordFile.active = self.settings.lastRecordWorksheetIndex
+        currentSheet = recordFile.active
+        # ['Start time', 'End time', 'Duration', 'Description', 'Tags', 'Tag-int']
+        record = [GetStoreDatetimeString(self.startDatetime), GetStoreDatetimeString(self.endDatetime), TrelloFormatTime(self.timeText.get()), 
+                  self.title.get(), '', ''] # TODO: tags and tag-int
+        currentSheet.append(record)
+        recordFile.save(filename=self.settings.latestRecordFilePath)
+        
+        self.settings.UpdateSettings(lastRecordedDatetime=datetime.today())
+        
     def Test(self):
         query = {
-            'key': key,
-            'token': token
+            'key': KEY,
+            'token': TOKEN
         }
         response = requests.get(
-            cardUrl,
+            CARD_URL,
             params=query
         )
         return min(response.json(), key=lambda x: x['pos'])['id']
 
 
-def FormatedTime(x):
+def TwoDigitNumber(x):
     if len(x) < 2:
         return "0" + x
     else:
@@ -122,7 +252,21 @@ def TrelloFormatTime(x : str):
     minutes += int(seconds >= 30)
     hours = str(hours + minutes // 60)
     minutes = str(minutes % 60)
-    return f'{FormatedTime(hours)}:{FormatedTime(minutes)}'
+    return f'{TwoDigitNumber(hours)}:{TwoDigitNumber(minutes)}'
+
+def GetClosestMonday():
+    startDay = datetime.today()
+    return (startDay - timedelta(days=startDay.weekday())).date()
     
+def CreateHeader(worksheet):
+    header = ['Start time', 'End time', 'Duration', 'Description', 'Tags', 'Tag-int']
+    worksheet.append(header)
     
+def GetStoreDatetimeString(t):
+    return datetime.strftime(t, '%Y-%m-%d %H:%M:%S.%f')
+
+def RetrieveDatetimeFromString(t):
+    return datetime.strptime(t, '%Y-%m-%d %H:%M:%S.%f')
+
+
 timer = Timer()
